65814fd7fc2aba5ad94c18e75ab602dc6c049653
root <root@DESKTOP-TA5P16G.localdomain>
2019-02-26 15:46:37 -0500

    asjd

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from routes import bp
 import pymongo
-
+#WHATS FOOD
 app = Flask(__name__)
 app.register_blueprint(bp)
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
