96360f7488e43281702184dfdeeb5ef4584178ca
root <root@nicole2019.cloud.compas.cs.stonybrook.edu>
2019-02-26 01:01:59 +0000

    nicole first commit

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from routes import bp
 #from pymongo import MongoClient
-
+############################################################
 
 app = Flask(__name__)
 app.register_blueprint(bp)
