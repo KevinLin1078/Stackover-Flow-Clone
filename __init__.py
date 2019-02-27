b2322856d5968399c2e709d78675598c4a50f916
root <root@kevin.cloud.compas.cs.stonybrook.edu>
2019-02-27 05:44:43 +0000

    Flask-CORS

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from routes import bp
 import pymongo
-#WHATS FOOD
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(bp)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 # mongo = PyMongo(app)
-print("mongo")
+
 
 if __name__ == '__main__':
 	app.debug=True
